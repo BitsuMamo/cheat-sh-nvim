f149a4676b12fb9d29b55cc3ed14400924f07f4b
Bitsu A. Mamo <54959510+BitsuAMamo@users.noreply.github.com>
2021-09-26 17:40:17 +0300

    Added capability of searching with Commands as well as the word under cursor

--- lua/cheat-sh-nvim/init.lua
+++ lua/cheat-sh-nvim/init.lua
@@ -10,37 +10,92 @@ local function openTerminal(command)
     return terimal
 end
 
--- Function to create and open window
-local function openWindow()
+-- Function to create window
+local function createWindow(bufHandler, width, height, widthOffset, heightOffset, row, col, type)
+    local window = vim.api.nvim_open_win(bufHandler, true ,{
+        relative=type,
+        width=width - widthOffset,
+        height=height - heightOffset,
+        row = row,
+        col = col,
+
+        border = {"╭", "─", "╮" ,"│", "╯", "─","╰", "│"}
+    })
+end
+
+-- Function to and open window
+local function openWindow(isCommand)
     local stats = vim.api.nvim_list_uis()[1]
     local width = stats.width
     local height = stats.height
 
-
+    -- create buffer for window
     local bufHandler = vim.api.nvim_create_buf(false, true)
-    local window = vim.api.nvim_open_win(bufHandler, true ,{
-        relative="cursor",
-        width=width - 40,
-        height=height - 40,
-        row = 0,
-        col = 2,
 
-        border = {"╭", "─", "╮" ,"│", "╯", "─","╰", "│"}
-    })
+    if isCommand then
+        createWindow(bufHandler, width, height, 10, 10, 5, 5, "editor")
+    else
+        createWindow(bufHandler, width, height, 40, 40, 0, 2, "cursor")
+    end
+
 
     -- Makes new windows follow same theme as the editor
     local windowOption = vim.api.nvim_win_set_option(window, 'winhl', 'Normal:Myhighlight')
 end
 
 -- Fucntion to open a window with all the cht.sh info for the word under the cursor
-local function cheatSheet()
+local function cheatSheetCursor()
     local fileType = vim.bo.filetype
     local wordUnderCursor = vim.fn.expand("<cword>")
 
     local command = "curl cht.sh/"..fileType.."/"..wordUnderCursor
     print(command)
-    local window = openWindow()
+    local window = openWindow(false)
     local terminal = openTerminal(command)
+end
+
+-- String split function. By default splits " "
+local function splitString(input, separator)
+    if separator == nil then
+        sep = "%s"
+    end
+
+    local list = {}
+    for str in string.gmatch(input, "([^"..separator.."]+)") do
+        table.insert(list,str)
+    end
+
+    return list
+end
+
+-- Function to open window with cheat sheet but search for a specific pharse
+local function cheatSheetCommand(input)
+    if input ~= nil then
+        local list = splitString(input, " ")
+
+        -- The first word is the language of choice
+        local language = list[1]
+
+        local searchPharse = ""
+        local numberOfWords = table.getn(list)
+        local counter = 2;
+        -- Creates the search pharse with the + concatenation for cht.sh
+        while counter <= numberOfWords do
+            if counter ~= numberOfWords then
+                searchPharse = searchPharse..list[counter].."+"
+            else
+                searchPharse = searchPharse..list[counter]
+            end
+            counter = counter + 1
+        end
+
+        local command = "curl cht.sh/"..language.."/"..searchPharse
+
+        local window = openWindow(true)
+        local termial = openTerminal(command)
+    else
+        print("Error. No input or Wrong input")
+    end
 end
 
 -- Function to dynamically resize window when referance window is resized
@@ -52,7 +107,8 @@ end
 
 -- Used to make functions that are only needed accesible
 return {
-    cheatSheet = cheatSheet,
+    cheatSheetCursor = cheatSheetCursor,
+    cheatSheetCommand = cheatSheetCommand,
     onResize = onResize,
 }
 
--- plugin/cheat-sh-nvim.vim
+++ plugin/cheat-sh-nvim.vim
@@ -1,11 +1,21 @@
 " Function made to dorp package and rerun everytime lua file changes
-fun! CheatSheetNvim()
+fun! CheatSheetCursor()
 
     lua for k in pairs(package.loaded) do if k:match("^cheat%-sh%-nvim") then package.loaded[k] = nil end end
-    lua require("cheat-sh-nvim").cheatSheet()
+    lua require("cheat-sh-nvim").cheatSheetCursor()
 endfun
 
-augroup CheatSheetNvim
+fun! CheatSheetCommand()
+    lua for k in pairs(package.loaded) do if k:match("^cheat%-sh%-nvim") then package.loaded[k] = nil end end
+    lua require("cheat-sh-nvim").cheatSheetCommand(vim.fn.input("Cheat Sheat> "))
+endfun
+
+augroup CheatSheetCursor
+    autocmd!
+    autocmd vimResized * :lua require("cheat-sh-nvim").onResize()
+augroup END
+
+augroup CheatSheetCommand
     autocmd!
     autocmd vimResized * :lua require("cheat-sh-nvim").onResize()
 augroup END
