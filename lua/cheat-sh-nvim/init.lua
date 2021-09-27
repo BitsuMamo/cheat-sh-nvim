8e0529b2f1442b0a9e1c523542e697438663ce4e
Bitsu A. Mamo <54959510+BitsuAMamo@users.noreply.github.com>
2021-09-27 12:25:47 +0300

    Major refactor, removing repeated code and renaming

--- lua/cheat-sh-nvim/init.lua
+++ lua/cheat-sh-nvim/init.lua
@@ -2,83 +2,82 @@
 --TODO: Added global fucntionality but can't chage values from init.vim
 --TODO: Add fucntionality of searching cht.sh from selected text
 
--- Function to open the termial with a command
-local function openTerminal(command)
-    local command_string = "terminal "..command
+-- Global Variables
+local stats
+local width
+local height
 
-    local terimal = vim.api.nvim_command(command_string)
-    return terimal
+-- Function to open the termial with cht.sh and a command
+local function openTerminal(command)
+    local command_string = "terminal curl cht.sh/"..command
+
+    vim.api.nvim_command(command_string)
 end
 
 -- Function to create window
-local function createWindow(bufHandler, width, height, widthRatio, heightRatio, row, col, type, anchor)
+local function createWindow(bufHandler, width, height, widthRatio, heightRatio, row, col, windowType, anchor)
     -- To avoid errors with negative width and height
     width = math.abs(math.floor(width * widthRatio))
     height = math.abs(math.floor(height * heightRatio))
-    if anchor == nil then
-        win_opts = {
-            relative=type,
-            width=width,
-            height=height,
-            row = row,
-            col = col,
+    local win_opts = {
+        relative = windowType,
+        width = width,
+        height = height,
+        row = row,
+        col = col,
 
-            border = {"╭", "─", "╮" ,"│", "╯", "─","╰", "│"}
+        border = {"╭", "─", "╮" ,"│", "╯", "─","╰", "│"},
+        anchor = anchor
     }
-    else
-        win_opts = {
-            relative=type,
-            width=width,
-            height=height,
-            row = row,
-            col = col,
 
-            border = {"╭", "─", "╮" ,"│", "╯", "─","╰", "│"},
-            anchor = anchor
-        }
-    end
-
-    local window = vim.api.nvim_open_win(bufHandler, true, win_opts)
+    return vim.api.nvim_open_win(bufHandler, true, win_opts)
 end
 
 -- Function to and open window
 local function openWindow(isCommand)
-    local stats = vim.api.nvim_list_uis()[1]
-    local width = stats.width
-    local height = stats.height
+    -- Gets the status of the whole window and not just the buffer
+    stats = vim.api.nvim_list_uis()[1]
+    -- Getting width and heigt for the status table
+    width = stats.width
+    height = stats.height
 
     -- create buffer for window
-    local bufHandler = vim.api.nvim_create_buf(false, true)
+    local bufferHandler = vim.api.nvim_create_buf(false, true)
+    local anchor = nil
+    local widthRatio, heightRatio, CursorColOffset, CursorRowOffset, windowType
 
     if isCommand then
-        local widthRatio = vim.g["CommandWidthRatio"]
-        local heightRatio = vim.g["CommandHeightRatio"]
-
-        createWindow(bufHandler, width, height, widthRatio, heightRatio, 5, 5, "editor", "NW")
+        widthRatio = vim.g["CommandWidthRatio"]
+        heightRatio = vim.g["CommandHeightRatio"]
+        CursorRowOffset = 5
+        CursorColOffset = 5
+        windowType = "editor"
     else
-        local widthRatio = vim.g["CursorWidthRatio"]
-        local heightRatio = vim.g["CursorHeightRatio"]
-
-        local CursorRowOffset = math.abs(vim.g["CursorRowOffset"])
-        local CursorColOffset = math.abs(vim.g["CursorColOffset"])
-
-        createWindow(bufHandler, width, height, widthRatio, heightRatio, CursorRowOffset, CursorColOffset, "cursor")
+        widthRatio = vim.g["CursorWidthRatio"]
+        heightRatio = vim.g["CursorHeightRatio"]
+        CursorRowOffset = math.abs(vim.g["CursorRowOffset"])
+        CursorColOffset = math.abs(vim.g["CursorColOffset"])
+        windowType = "cursor"
     end
 
+    local window = createWindow(bufferHandler, width, height, widthRatio, heightRatio, CursorRowOffset, CursorColOffset, windowType, anchor)
 
     -- Makes new windows follow same theme as the editor
-    local windowOption = vim.api.nvim_win_set_option(window, 'winhl', 'Normal:Myhighlight')
+    vim.api.nvim_win_set_option(window, 'winhl', 'Normal:Myhighlight')
+
 end
 
 -- Fucntion to open a window with all the cht.sh info for the word under the cursor
 local function cheatSheetCursor()
+
     local fileType = vim.bo.filetype
     local wordUnderCursor = vim.fn.expand("<cword>")
 
-    local command = "curl cht.sh/"..fileType.."/"..wordUnderCursor
+    local command = fileType.."/"..wordUnderCursor
     print(command)
-    local window = openWindow(false)
-    local terminal = openTerminal(command)
+    openWindow(false)
+    openTerminal(command)
+
 end
 
 -- String split function. By default splits " "
@@ -87,39 +86,39 @@ local function splitString(input, separator)
         sep = "%s"
     end
 
-    local list = {}
+    local wordList = {}
     for str in string.gmatch(input, "([^"..separator.."]+)") do
-        table.insert(list,str)
+        table.insert(wordList,str)
     end
 
-    return list
+    return wordList
 end
 
 -- Function to open window with cheat sheet but search for a specific pharse
 local function cheatSheetCommand(input)
     if input ~= nil then
-        local list = splitString(input, " ")
+        local wordList = splitString(input, " ")
 
         -- The first word is the language of choice
-        local language = list[1]
+        local language = wordList[1]
 
         local searchPharse = ""
-        local numberOfWords = table.getn(list)
+        local numberOfWords = table.getn(wordList)
         local counter = 2;
         -- Creates the search pharse with the + concatenation for cht.sh
         while counter <= numberOfWords do
             if counter ~= numberOfWords then
-                searchPharse = searchPharse..list[counter].."+"
+                searchPharse = searchPharse..wordList[counter].."+"
             else
-                searchPharse = searchPharse..list[counter]
+                searchPharse = searchPharse..wordList[counter]
             end
             counter = counter + 1
         end
 
-        local command = "curl cht.sh/"..language.."/"..searchPharse
+        local command = language.."/"..searchPharse
 
-        local window = openWindow(true)
-        local termial = openTerminal(command)
+        window = openWindow(true)
+        termial = openTerminal(command)
     else
         print("Error. No input or Wrong input")
     end
@@ -127,9 +126,9 @@ end
 
 -- Function to dynamically resize window when referance window is resized
 local function onResize()
-    local stats = vim.api.nvim_list_uis()[1]
-    local width = stats.width
-    local height = stats.height
+    stats = vim.api.nvim_list_uis()[1]
+    width = stats.width
+    height = stats.height
 end
 
 -- Used to make functions that are only needed accesible
@@ -138,4 +137,3 @@ return {
     cheatSheetCommand = cheatSheetCommand,
     onResize = onResize,
 }
-
